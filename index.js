20d0152968a94b9720326f5a5dce3a1e9220ca7f
Ahmet Emre Kılınç <aekilinc@gmail.com>
2018-04-03 10:53:58 +0300

    updating fork

--- index.js
+++ index.js
@@ -4,11 +4,18 @@
 module.exports = {
   name: 'ember-cli-data-export',
 
+  isDevelopingAddon : function(){
+    return true;
+  },
+  hintingEnabled: function() {
+    return false;
+  },
+
   included: function(app) {
     this._super.included(app);
     app.import('vendor/Blob.js');
     app.import('vendor/FileSaver-1.3.3.js');
     app.import('vendor/jszip-0.10.8.js');
-    app.import('vendor/xlsx-0.10.8.js');
+    app.import('vendor/xlsx.core.min.js');
   }
 };
